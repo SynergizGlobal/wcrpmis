d1ff42320945f8c8b0e275947b9b450542b67f8c
Shanmukh Venkatesh <syntrack@synergizglobal.com>
2025-11-19 12:57:35 +0530

    keyword added

--- wcrfrontend/src/components/UpdateForms/LandAcquisition/LandAcquisition.jsx
+++ wcrfrontend/src/components/UpdateForms/LandAcquisition/LandAcquisition.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { RefreshContext } from "../../../context/RefreshContext";
 import Select from "react-select";
 import { CirclePlus, Download } from "lucide-react";
