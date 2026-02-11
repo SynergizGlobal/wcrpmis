a671cfcada45594bd77413d0441b2dd9a830d548
Aman-SynergizGlb <aman.thakur@synergizglobal.com>
2026-02-11 16:27:40 +0530

    DMS CommonUtil class added

---
+++ wcrbackend/src/main/java/com/wcr/dms/common/CommonUtil.java
@@ -0,0 +1,28 @@
+package com.wcr.dms.common;
+
+import com.wcr.wcrbackend.entity.User;
+
+public class CommonUtil {
+
+	public static String getExtensionFromContentType(String contentType) {
+	    switch (contentType) {
+	        case "image/jpeg": return "jpg";
+	        case "image/png": return "png";
+	        case "application/pdf": return "pdf";
+	        case "application/msword": return "doc";
+	        case "application/vnd.openxmlformats-officedocument.wordprocessingml.document": return "docx";
+	        case "application/vnd.ms-excel": return "xls";
+	        case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": return "xlsx";
+	        case "text/plain": return "txt";
+	        case "text/html": return "html";
+	        case "application/zip": return "zip";
+	        // add more as needed
+	        default: return "bin"; // fallback
+	    }
+	}
+	
+	public static boolean isITAdminOrSuperUser(User user) {
+		return user.getUserRoleNameFk().equalsIgnoreCase("IT Admin") || user.getUserRoleNameFk().equalsIgnoreCase("Super user");
+	}
+}
+
