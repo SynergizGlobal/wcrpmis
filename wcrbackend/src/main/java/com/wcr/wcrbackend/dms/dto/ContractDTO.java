d515ceaf57303acd77ae874c8d163b0eceb1a10b
Aman-SynergizGlb <aman.thakur@synergizglobal.com>
2026-02-12 18:21:37 +0530

    package name change in dms

---
+++ wcrbackend/src/main/java/com/wcr/wcrbackend/dms/dto/ContractDTO.java
@@ -0,0 +1,15 @@
+package com.wcr.wcrbackend.dms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContractDTO {
+	private String id;
+    private String name;
+}
